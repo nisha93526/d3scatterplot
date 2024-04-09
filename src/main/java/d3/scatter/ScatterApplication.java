54a2ef461a65a869871382abd1cc7604f09bed87
nisha93526 <nisha93526@gmail.com>
2024-04-10 01:48:36 +0530

    Initial commit

---
+++ src/main/java/d3/scatter/ScatterApplication.java
@@ -0,0 +1,13 @@
+package d3.scatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScatterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/d3/scatter/ScatterApplicationTests.java
@@ -0,0 +1,13 @@
+package d3.scatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
